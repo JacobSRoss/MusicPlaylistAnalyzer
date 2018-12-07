deb74680962f095f5bd25219c163a86d15c05dfd
Jacob Ross <jross1818@gmail.com>
2018-12-06 23:05:17 -0600

    added full funcionality for getting data from file and pushing data out to report

---
+++ MusicPlaylistAnalyzer/MusicStatsReport.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlaylistAnalyzer
+{
+    class MusicStatsReport
+    {
+        public static string GenerateText(List<MusicStats> musicStatsList)
+        {
+            string report = "Music Playlist Analyzer Report \r\n\r\n";
+
+            if (musicStatsList.Count() < 1)
+            {
+                Console.WriteLine("musicStatsList.Count: " + musicStatsList.Count());
+                report += "No data is available.\r\n";
+
+                return report;
+            }
+
+            report += $"Songs that received 200 or more plays: \r\n";
+            var amountOfPlays = from musicStats in musicStatsList where musicStats.Plays > 200 select new { musicStats.Name, musicStats.Artist, musicStats.Album, musicStats.Genre, musicStats.Size, musicStats.Time, musicStats.Year, musicStats.Plays };
+            if (amountOfPlays.Count() > 0)
+            {
+                foreach (var song in amountOfPlays)
+                {
+                    report += "Name: " + song.Name + ", Artist: " + song.Artist + ", Album: " + song.Album + ", Genre: " + song.Genre + ", Size: " + song.Size + ", Time: " + song.Time + ", Year: " + song.Year + ", Plays: " + song.Plays;
+                    report += "\r\n";
+                }
+            } else
+            {
+                report += "Not available. \n";
+            }
+            
+
+
+
+
+
+            return report;
+        }
+        
+    }
+}
--- MusicPlaylistAnalyzer/Program.cs
+++ MusicPlaylistAnalyzer/Program.cs
@@ -10,6 +10,38 @@ namespace MusicPlaylistAnalyzer
     {
         static void Main(string[] args)
         {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Please use the correct amount of arguments: MusicPlaylistAnalyzer <music_playlist_file_path> <report_file_path>");
+                Console.ReadLine();
+                Environment.Exit(1);
+            }
+
+            string musicDataFilePath = args[0];
+            string reportFilePath = args[1];
+
+            List<MusicStats> musicStatsList = null;
+
+            try
+            {
+                musicStatsList = MusicStatsLoader.Load(musicDataFilePath);
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+                Environment.Exit(2);
+            }
+
+            var report = MusicStatsReport.GenerateText(musicStatsList);
+
+            try
+            {
+                System.IO.File.WriteAllText(reportFilePath, report);
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Environment.Exit(3);
+            }
 
         }
     }
